0984e4307d462b6dcd76d3e9f35242b599a80177
TinnyDatta <tinnydatta.prof@gmail.com>
2024-03-13 12:30:08 +0600

    final commit

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({blog, handleAddToBookmark}) => {
+const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
     const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
         <div className='mb-20'>
@@ -25,13 +25,17 @@ const Blog = ({blog, handleAddToBookmark}) => {
                     hashtags.map((hash, idx) => <span key={idx}> <a href="">#{hash}</a></span>)
                 }
             </p>
+            <button
+            onClick={() => handleMarkAsRead (reading_time)}
+             className='text-purple-800 font-bold underline'>Mark As Read</button>
         </div>
     );
 };
 
 Blog.propTypes = {
     blog:PropTypes.object.isRequired,
-    handleAddToBookmark:PropTypes.func.isRequired
+    handleAddToBookmark:PropTypes.func.isRequired,
+    handleMarkAsRead : PropTypes.func.isRequired
 
 }
 
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types'
 import Bookmark from '../Bookmark/Bookmark';
 
-const Bookmarks = ({bookmarks}) => {
+const Bookmarks = ({bookmarks, readingTime}) => {
     return (
+        
         <div className="md:w-1/3 bg-slate-300 ml-4 mt-3 pt-4">
+           <h3 className="text-4xl bg-slate-500 text-center">Reading Time : {readingTime}</h3>
             <h2 className="text-3xl text-center">Bookmarked Blogs : {bookmarks.length}</h2>
             {
                 bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
@@ -13,7 +15,8 @@ const Bookmarks = ({bookmarks}) => {
 };
 
 Bookmarks.propTypes = {
-    bookmarks: PropTypes.array
+    bookmarks: PropTypes.array,
+    readingTime : PropTypes.number
 }
 
 export default Bookmarks;
