dbafd203217a74ba8112a2d4aab394e2d31a554b
TinnyDatta <tinnydatta.prof@gmail.com>
2024-03-13 10:46:57 +0600

    added bookmarked section

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,38 @@
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
-    console.log(blog);
+import { FaBookmark } from 'react-icons/fa';
+
+const Blog = ({blog, handleAddToBookmark}) => {
+    const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='rounded-2xl w-full' src={cover} alt={`cover picture of the image ${title}`} />
+            <div className='flex justify-between mt-8'>
+                <div className='flex'>
+                    <img className='w-16 h-16 rounded-full' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)} className='ml-2'><FaBookmark></FaBookmark></button>
+                </div>
+            </div>
+            <h2 className='text-4xl my-4'> {title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}> <a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog:PropTypes.object.isRequired
+    blog:PropTypes.object.isRequired,
+    handleAddToBookmark:PropTypes.func.isRequired
+
 }
 
 export default Blog;
